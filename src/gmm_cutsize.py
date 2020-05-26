e351479b645aa46fb28ce7024b548eb7d3eb1e1a
Kimberly Ding <dingkimberly@gmail.com>
2020-05-26 00:17:15 -0400

    removed print statements

--- src/gmm_cutsize.py
+++ src/gmm_cutsize.py
@@ -49,22 +49,17 @@ def solve_cut(L, W, k, rho):
     for j in range(n):
         nodes.append(model.addVar(vtype=GRB.BINARY))
 
-    print("added variables")
     # Want to maximize x * W
     obj = gp.LinExpr()
     for i in range(n):
         obj += nodes[i] * W[i]/math.sqrt(k)
     model.setObjective(obj, sense=GRB.MAXIMIZE)
     
-    print("added obj")
-    
     # Only k nodes in the subgraph allowed
     expr = gp.LinExpr()
     for i in range(n):
         expr += nodes[i]
     model.addConstr(expr, GRB.LESS_EQUAL, k)
-    
-    print("added constr")
     
     # Subgraph must have cut < rho
     # 1/4 * x * L * x
@@ -75,11 +70,8 @@ def solve_cut(L, W, k, rho):
             expr2 += curr_x * L[i][j] * (nodes[j] - 0.5)
     model.addConstr(expr2, GRB.LESS_EQUAL, rho)
     
-    print("added constr2")
-    
     model.optimize()
     
-    print("optimized")
     
     count = 0
     scores = [None for _ in range(n)]
