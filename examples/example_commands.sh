41298f4562189f6c23a44154ecfcfee025225dd9
Kimberly Ding <dingkimberly@gmail.com>
2020-05-28 23:26:10 -0400

    .

--- examples/example_commands.sh
+++ examples/example_commands.sh
@@ -20,16 +20,16 @@ echo "Finding anomalies..."
 
 
 # Find submatrix anomaly.
-#python3 $scripts/find_anomaly.py \
-#    -i $data/submatrix_scores.hdf5 \
-#    -a submatrix \
-#    -o $results/submatrix_results.tsv 
+python3 $scripts/find_anomaly.py \
+    -i $data/submatrix_scores.hdf5 \
+    -a submatrix \
+    -o $results/submatrix_results.tsv 
 
 # Find line anomaly.
-#python3 $scripts/find_anomaly.py \
-#    -i $data/line_scores.tsv \
-#    -a line \
-#    -o $results/line_results.tsv
+python3 $scripts/find_anomaly.py \
+    -i $data/line_scores.tsv \
+    -a line \
+    -o $results/line_results.tsv
 
 # Find cutsize anomaly.
 python3 $scripts/find_anomaly.py \
@@ -38,9 +38,8 @@ python3 $scripts/find_anomaly.py \
     -o $results/cutsize_results.tsv \
     -elf $data/cutsize_graph.tsv -r 20
     
-
 # Find unconstrained anomaly.
-#python3 $scripts/find_anomaly.py \
-#    -i $data/cutsize_scores.tsv \
-#    -a unconstrained \
-#    -o $results/unconstrained_results.tsv
+python3 $scripts/find_anomaly.py \
+    -i $data/cutsize_scores.tsv \
+    -a unconstrained \
+    -o $results/unconstrained_results.tsv
